88bd2c6b81221363906954746789162b3c0f1c33
wqc吴芊铖 <qcwu@Ctrip.com>
2019-07-06 13:18:47 +0800

    接入数据

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,15 +1,20 @@
 Page({
-    data: {
-        id: 1,
-        name: '孙兴慜 Ins',
-        avatar: 'https://tva4.sinaimg.cn/crop.0.0.640.640.180/006mMONSjw8f07e97juukj30hs0hsgls.jpg',
-        content:
-            'We win together we lose together that is a Team I’m so proud of being in this Team We had a...... ',
-        time: '06-04 00:08',
-        images: ['https://wx1.sinaimg.cn/mw690/006mMONSly1g3of2lks1lj30sg0jh76n.jpg']
+    data: {},
+
+    onLoad(option) {
+        this.id = option.id
+        this.fetchDetail()
     },
 
-    onLoad(option) {},
+    onShareAppMessage() {},
 
-    onShareAppMessage() {}
+    fetchDetail() {
+        wx.request({
+            url: 'https://api.spursnews.net/api/v1/feeds/' + this.id,
+            success: (res) => {
+                let detailData = res.data.data
+                this.setData(detailData)
+            }
+        })
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,52 +1,29 @@
 Page({
-  data: {
-      InfoList: [
-    {
-      id: 1,
-      name: '孙兴慜 Ins',
-      avatar: 'https://tva4.sinaimg.cn/crop.0.0.640.640.180/006mMONSjw8f07e97juukj30hs0hsgls.jpg',
-      content: 'We win together we lose together that is a Team I’m so proud of being in this Team We had a...... ',
-      time: '06-04 00:08',
-      images: ['https://wx1.sinaimg.cn/mw690/006mMONSly1g3of2lks1lj30sg0jh76n.jpg']
+    data: {},
+
+    onLoad(option) {
+        this.fetchFeeds()
     },
-    {
-      id: 2,
-      name: '热刺 Ins',
-      avatar: 'https://tva4.sinaimg.cn/crop.0.0.640.640.180/006mMONSjw8f07e97juukj30hs0hsgls.jpg',
-      content: 'The journey was incredible, the memories will live with us forever. It wasn’t to be this time, but...... ',
-      time: '06-02 05:53',
-      images: ['https://wx1.sinaimg.cn/mw690/006mMONSly1g3mdt4jtvhj30u00nkwk7.jpg']
+
+    onShareAppMessage() {},
+
+    fetchFeeds() {
+        wx.showLoading()
+        wx.request({
+            url: 'https://api.spursnews.net/api/v1/feeds',
+            success: (res) => {
+                let feeds = res.data.data
+                this.setData({ feeds })
+                wx.hideLoading()
+            }
+        })
     },
-    {
-      id: 3,
-      name: '孙兴慜 Ins',
-      avatar: 'https://tva4.sinaimg.cn/crop.0.0.640.640.180/006mMONSjw8f07e97juukj30hs0hsgls.jpg',
-      content: '测试中文展示测试中文展示测试中文展示测试中文展示测试中文展示 ',
-      time: '06-04 00:08',
-      images: []
-    },
-    {
-      id: 4,
-      name: '热刺 Ins',
-      avatar: 'https://tva4.sinaimg.cn/crop.0.0.640.640.180/006mMONSjw8f07e97juukj30hs0hsgls.jpg',
-      content: '',
-      time: '06-02 05:53',
-      images: ['https://wx1.sinaimg.cn/mw690/006mMONSly1g3mdt4jtvhj30u00nkwk7.jpg']
+
+    handleGoDetail(event) {
+        let id = event.currentTarget.dataset.id
+        let params = {
+            url: '../detail/index?id=' + id
+        }
+        wx.navigateTo(params)
     }
-  ]
-  },
-
-  onLoad(option) {
-  },
-
-  onShareAppMessage() {
-
-  },
-
-  handleGoDetail() {
-      let params = {
-        url: '../detail/index'
-      }
-      wx.navigateTo(params)
-  }
 })
